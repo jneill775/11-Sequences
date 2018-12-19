3549c41db0c176115f8d30b1169187e4e8e15842
jneill775 <neilljs@rose-hulman.edu>
2018-12-19 16:12:19 -0500

    m4 done

--- src/m4_more_iterating.py
+++ src/m4_more_iterating.py
@@ -36,8 +36,8 @@ def main():
     """ Calls the   TEST   functions in this module. """
     #run_test_sum_sequence()
     #run_test_count_items_bigger_than()
-    run_test_count_positive_sines()
-    # run_test_sum_first_n()
+    #run_test_count_positive_sines()
+    run_test_sum_first_n()
 
 
 def run_test_sum_sequence():
@@ -438,7 +438,7 @@ def count_items_bigger_than(numbers, threshold):
 def run_test_count_positive_sines():
     """ Tests the   count_positive_sines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  count_positive_sines  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond what we supplied.
     #
@@ -533,10 +533,25 @@ def run_test_count_positive_sines():
     print('Test 10 expected:', expected)
     print('        actual:  ', actual)
 
-    # TO DO 6 (continued):  Add your 1 ADDITIONAL test here:
+    # DONE 6 (continued):  Add your 1 ADDITIONAL test here:
+
+    # Test 11:
+    expected = 6
+    actual = count_positive_sines([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+    print()
+    print('Test 11 expected:', expected)
+    print('       actual:  ', actual)
 
 
 def count_positive_sines(numbers):
+
+    ibethedoesntkissya = 0
+
+    for k in range(len(numbers)):
+        if math.sin(numbers[k]) > 0:
+            ibethedoesntkissya += 1
+    return ibethedoesntkissya
+
     """
     What comes in:  An sequence of numbers.
     What goes out: Returns the number of items in the given sequence
@@ -560,7 +575,7 @@ def count_positive_sines(numbers):
       :type sequence: list or tuple (of numbers)
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
 
@@ -568,7 +583,7 @@ def count_positive_sines(numbers):
 def run_test_sum_first_n():
     """ Tests the   sum_first_n   function. """
     # -------------------------------------------------------------------------
-    # TODO: 8. Implement this TEST function.
+    # DONE: 8. Implement this TEST function.
     #   It TESTS the  sum_first_n  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests.
     #
@@ -637,10 +652,29 @@ def run_test_sum_first_n():
     print('Test 7 expected:', expected)
     print('       actual:  ', actual)
 
-    # TO DO 8 (continued):  Add your 2 ADDITIONAL tests here:
+    # DONE 8 (continued):  Add your 2 ADDITIONAL tests here:
+
+    # Test 8:
+    expected = -52
+    actual = sum_first_n([4, -56, 3560, 257], 2)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual:  ', actual)
+
+    # Test 9:
+    expected = -31
+    actual = sum_first_n([2, 24, 56, -113, 50, 534], 4)
+    print()
+    print('Test 9 expected:', expected)
+    print('       actual:  ', actual)
 
 
 def sum_first_n(numbers, n):
+    mwah = 0
+    for k in range(n):
+        mwah = mwah + numbers[k]
+    return mwah
+
     """
     What comes in:
       -- An sequence of numbers.
@@ -662,7 +696,7 @@ def sum_first_n(numbers, n):
       :type n: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 9. Implement and test this function.
+    # DONE: 9. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # RESTRICTION:
